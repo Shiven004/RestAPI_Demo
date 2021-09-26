b4fb9d942c7f4018307cf26554168623bc0a1807
Shivender Khajuria <shivenkhajuria@yahoo.com>
2021-09-26 21:47:09 +1000

    Add files via upload
    First Commit

---
+++ Wipro_technical_task/app/fetch_data_op.py
@@ -0,0 +1,39 @@
+"""
+Description: This module invokes read process to fetch lab data 
+             and perform different fetch operations based on below scenarios:
+                 1. Fetch lab centre data based on lab id
+                 2. Generate patient id and associate lab centre booked.
+
+
+Author:     Shivender Khajuria           
+Dated:      25-09-21
+Filename:   fetch_data_op.py
+Version:    1.0
+Purpose:    Wipro Tech Test
+
+"""
+from read_write_op import read_lab_details
+import random
+from read_write_op import read_lab_details
+
+def fetch_lab_details(lab_id):    
+    lab_centre_details = []
+    lab_data = read_lab_details()
+    for lab in lab_data:        
+        if lab['lab_id'] == lab_id:
+            lab_centre_details.append(lab)
+    return lab_centre_details
+
+def gen_patient_details(lab_id,test_requested):
+    lab_centre_details = fetch_lab_details(lab_id)    
+    patient_data = {}
+    # Generate unique patient_id for the patient.
+    if  lab_centre_details:
+        patient_id = "PAT_" + lab_id +"_" + str(random.randint(1111,9999))
+        patient_data["Patient_ID"] = patient_id  
+    #fetch details from lab_centre_details list.
+    for item in lab_centre_details:        
+        patient_data["Lab_ID"] = item["lab_id"]
+        patient_data["Lab_Type"] = item["lab_type"]
+        patient_data["Test_Name"] = test_requested     
+    return patient_data
---
+++ Wipro_technical_task/app/read_write_op.py
@@ -0,0 +1,47 @@
+"""
+Description: This module reads the data from json files present in data directory
+             and perform different fetch operations based on below scenarios.             
+
+Author:     Shivender Khajuria           
+Dated:      25-09-21
+Filename:   read_write_op.py
+Version:    1.0
+Purpose:    Wipro Tech Test
+
+"""
+import json 
+
+#Global variables
+dir_path = '../app/data/'
+filename_lab_data = 'lab_centres.json'
+filename_patient = 'patient_details.json'
+
+
+# Funtion performs read operations on the lab_centres.json
+def read_lab_details():
+    print("Performing read operation - input order details now....")
+    #Orders details json deserialize operation.
+    with open(dir_path + filename_lab_data, "r") as read_file:
+        try:                  
+            lab_details  = json.load(read_file)
+        except:
+            print("Please check orders_details.json file format....\n")
+            read_file.seek(0)
+            lab_details = json.loads('['+read_file.read().replace('}{','},{')+']')[0]
+        finally:
+            print("Exiting read_orders_details process now...")   
+    return lab_details
+
+
+# Funtion performs write operation on lab_response_data.json.
+def write_patient_details(patient_details):
+    print("Performing write operation - unique orders now....")
+    #Orders details json deserialize operation.
+    with open(dir_path + filename_patient, "a+") as write_file:
+        try:                  
+            json.dump(patient_details, write_file, indent=4)            
+        except IOError:
+            print("Error while writing to unique_orders.json file....\n")
+        finally:
+            print("Exiting write_unique_details process now...")           
+    return
